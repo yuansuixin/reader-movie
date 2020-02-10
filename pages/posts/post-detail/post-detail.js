3ca3cd78a06a80ba54f47f7b7c86d36176b27cb8
macaoyuan <macaoyuan@peiwo.cn>
2020-02-10 22:08:26 +0800

    feat: 收藏 finish

--- pages/posts/post-detail/post-detail.js
+++ pages/posts/post-detail/post-detail.js
@@ -25,6 +25,24 @@ Page({
     }
   },
   onCollectionTap:function(event) {
+    // this.getPostsCollectedAsy();
+    this.getPostsCollectedSyc();
+  },
+  getPostsCollectedAsy:function(){
+    var that = this;
+    wx.getStorage({
+      key: "posts_collected",
+      success: function (res) {
+        var postsCollected = res.data;
+        var postCollected = postsCollected[that.data.currentPostId];
+        // 收藏变成未收藏，未收藏变成收藏
+        postCollected = !postCollected;
+        postsCollected[that.data.currentPostId] = postCollected;
+        that.showToast(postsCollected, postCollected);
+      }
+    })
+  },
+  getPostsCollectedSyc:function() {
     var postsCollected = wx.getStorageSync('posts_collected');
     console.log('postCollected==0', postsCollected)
     console.log('this.data.currentPostId=', this.data.currentPostId)
@@ -32,11 +50,51 @@ Page({
     postCollected = !postCollected;
     postsCollected[this.data.currentPostId] = postCollected;
     console.log('postCollected==', postCollected)
+    this.showToast(postsCollected, postCollected)
+  },
+  showModal: function (postsCollected, postCollected) {
+    var that = this;
+    wx.showModal({
+      title: '收藏',
+      content: postCollected?'收藏该文章?':'取消收藏该文章？',
+      showCancel: 'true',
+      cancelText: '取消',
+      cancelColor: '#333',
+      confirmText: '确认',
+      confirmColor: '#405f80',
+      success:function(res){
+        if(res.confirm){
+          // 更新文章是否收藏的缓存值
+          wx.setStorageSync('posts_collected', postsCollected);
+          // 更新数据绑定，从而实现切换图片
+          that.setData({
+            collected: postCollected
+          })
+        }
+      }
+    })
+  },
+  showToast: function (postsCollected, postCollected) {
     // 更新文章是否收藏的缓存值
     wx.setStorageSync('posts_collected', postsCollected);
     // 更新数据绑定，从而实现切换图片
     this.setData({
-      collected: postsCollected
+      collected: postCollected
+    })
+    wx.showToast({
+      title: postCollected ? '收藏成功' : '取消收藏',
+      duration: 1000,
+      icon: 'success'
+    })
+  },
+  onShareTap:function(eve  nt) {
+    wx.showActionSheet({
+      itemList: ['分享给为微信好友', '分享到朋友圈', '分享到QQ','分享到微博'],
+      itemColor: '#405f80',
+      success:function(res) {
+        // res.cancel 用户是不是点击了取消按钮
+        // res.tapIndex 数组元素的序号，从0开始
+      }
     })
   }
 })
