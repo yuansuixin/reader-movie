a310b0e189edd754f0dd2e42aa81b8e971733bcc
macaoyuan <macaoyuan@peiwo.cn>
2020-02-10 11:50:40 +0800

    feat: news list

---
+++ pages/posts/posts.js
@@ -0,0 +1,71 @@
+
+// 只能用相对路径，不能使用绝对路径
+var postsData = require("../../data/posts_data.js");
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    //参数必须是一个对象
+    // this.setData({posts_content}); //es6语法，key和value的名字一致可以简化
+    this.setData({
+      posts_key:postsData.postList
+      });
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  }, 
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
