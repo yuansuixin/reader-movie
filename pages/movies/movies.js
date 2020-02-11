c09974caf43729289d7fecc198dd6d4c2cc1ac19
macaoyuan <macaoyuan@peiwo.cn>
2020-02-11 22:26:03 +0800

    feat: movie data

--- pages/movies/movies.js
+++ pages/movies/movies.js
@@ -1,66 +1,66 @@
 // pages/movies/movies.js
+var util = require('../../utils/util.js');
+var app = getApp();
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  data:{
+    //异步请求绑定对象的时候需要有初始值，否则找不到
+    inTheaters:{},
+    comingSoon:{},
+    top250:{}
   },
-
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    var baseUrl = app.globalData.doubanBase;
+    var inTheatersUrl = '/v2/movie/in_theaters'+"?start=0&count=3";
+    var comingSoonUrl = '/v2/movie/coming_soon' + "?start=0&count=3";
+    var top250Url = '/v2/movie/top250' + "?start=0&count=3";
+
+    this.getMovieListData(baseUrl + inTheatersUrl, "inTheaters", '正在热映');
+    this.getMovieListData(baseUrl + comingSoonUrl,'comingSoon','即将上映');
+    this.getMovieListData(baseUrl + top250Url,'top250','豆瓣Top250');
 
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  getMovieListData: function (url, settedKey, cagetoryTitle){
+    var that = this;
+    // 是异步调用
+    wx.request({
+      url: url,
+      method: 'GET',
+      header: {
+        "Content-Type": 'application/json'
+      },
+      success: function (res) {
+        console.log(res)
+        that.processDoubanData(res.data, settedKey, cagetoryTitle);
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  processDoubanData: function (moviesDouban, settedKey,cagetoryTitle){
+    var movies = [];
+    for(var idx in moviesDouban.subjects){
+      var subject = moviesDouban.subjects[idx];
+      var title = subject.title;
+      if(title.length>=6){
+        title = title.substring(0,6) + '...';
+      }
+      var temp = {
+        stars: util.convertToStarsArray(subject.rating.stars),
+        title,
+        average:subject.rating.average,
+        coverageUrl: subject.images.large,
+        movieId:subject.id
+      }
+      console.log('temp===',temp)
+      console.log(subject.rating.stars)
+      movies.push(temp)
+    }
+    console.log('movies==', movies)
+    //动态属性
+    var readyData = {};
+    readyData[settedKey] = { movies, cagetoryTitle};
+    this.setData(readyData);
   }
 })
