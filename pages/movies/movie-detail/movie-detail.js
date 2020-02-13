e0538c645710756f21a76e2247b94d48b61d537d
macaoyuan <macaoyuan@peiwo.cn>
2020-02-13 12:36:18 +0800

    fix: es6

--- pages/movies/movie-detail/movie-detail.js
+++ pages/movies/movie-detail/movie-detail.js
@@ -1,5 +1,5 @@
+import {Movie} from 'class/Movie.js';
 var app = getApp()
-var util = require('../../../utils/util.js')
 Page({
   data: {
     movie:{}
@@ -8,45 +8,17 @@ Page({
     var movieId = options.id;
     var baseUrl = app.globalData.doubanBase;
     var url = baseUrl + '/v2/movie/subject/'+ movieId;
-    util.http(url, this.processDoubanData)
-  },
-  processDoubanData:function(data){
-    var director = {
-      avatar:'',
-      name:'',
-      id:''
-    }
-    if (data.directors[0] != null) {
-      if (data.directors[0].avatars != null) {
-        director.avatar = data.directors[0].avatars.large
-
-      }
-      director.name = data.directors[0].name;
-      director.id = data.directors[0].id;
-    }
-    var movie = {
-      movieImg: data.images ? data.images.large : "",
-      country: data.countries[0],
-      title: data.title,
-      originalTitle: data.original_title,
-      wishCount: data.wish_count,
-      commentCount: data.comments_count,
-      year: data.year,
-      generes: data.genres.join("、"),
-      stars: util.convertToStarsArray(data.rating.stars),
-      score: data.rating.average,
-      director: director,
-      casts: util.convertToCastString(data.casts),
-      castsInfo: util.convertToCastInfos(data.casts),
-      summary: data.summary
-    }
-    this.setData({movie})
-  },
-  viewMoviePostImg:function(event){
-    var src = event.currentTarget.dataset.src;
-    wx.previewImage({
-      current:src,
-      urls: [src],
+    var movie = new Movie(url);
+    movie.getMovieData((movie)=>{
+      this.setData({ movie })
     })
-  }
+  },
+  /*查看图片*/
+  viewMoviePostImg: function (e) {
+    var src = e.currentTarget.dataset.src;
+    wx.previewImage({
+      current: src, // 当前显示图片的http链接
+      urls: [src] // 需要预览的图片http链接列表
+    })
+  },
 })
